744893eae90c3d978bb29556bd5940e9ae051d8b
Amar MATALLAH <a.matallah767@gmail.com>
2022-07-08 10:54:44 +0200

    initial-commit

--- src/ExperienceWebGL/World.js
+++ src/ExperienceWebGL/World.js
@@ -1,12 +1,13 @@
+import * as THREE from 'three'
+import './style/style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+
 export default class World
 {
     constructor()
     {
-       /**
-        * Scene
-        */
-        const scene = new THREE.Scene()
-        console.log(scene)
+        
         /**
          * Sizes
          */
@@ -14,38 +15,59 @@ export default class World
         sizes.width = window.innerWidth
         sizes.height = window.innerHeight
         
-        /**
-         * Resize
-         */
         window.addEventListener('resize', () =>
         {
-            //Update sizes object
+            // Save sizes
             sizes.width = window.innerWidth
             sizes.height = window.innerHeight
-
+        
             // Update camera
             camera.aspect = sizes.width / sizes.height
             camera.updateProjectionMatrix()
-
+        
             // Update renderer
             renderer.setSize(sizes.width, sizes.height)
-
         })
-
+        
         /**
-         * Camera
+         * Environnements
          */
-
-        //Base camera
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+        // Scene
+        const scene = new THREE.Scene()
+        
+        // Camera
+        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
         camera.position.z = 3
         scene.add(camera)
-
+        
+        // Test
+        const cube = new THREE.Mesh(new THREE.BoxBufferGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
+        scene.add(cube)
+        
+        // Renderer
+        const renderer = new THREE.WebGLRenderer({
+            canvas: document.querySelector('.webgl'),
+            alpha: true
+        })
+        renderer.setPixelRatio(window.devicePixelRatio)
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.domElement.classList.add('webgl')
+        document.body.appendChild(renderer.domElement)
+        
         /**
-         * Lights
+         * Loop
          */
-        const light = new THREE.AmbientLight( 0x404040 )
-        scene.add( light )
-
+        const loop = () =>
+        {
+            // Update
+            cube.rotation.y += 0.01
+        
+            // Render
+            renderer.render(scene, camera)
+        
+            // Keep looping
+            window.requestAnimationFrame(loop)
+        }
+        loop()
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three'
 import World from './ExperienceWebGL/World.js'
-import styles from './style/style.css';
+import './style/style.css';
 
 const world = new World()
-console.log(world)
